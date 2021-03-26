0a0197a1a0e28a6e84ef3d6353f973d8163b7a5a
Filyan <filyan.karagoz@mercadolibre.com>
2021-03-26 16:29:53 -0300

    Adding translate morse code

---
+++ 01-Spring/src/main/java/com/example/demo/Entities/Message.java
@@ -0,0 +1,12 @@
+package com.example.demo.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Message {
+    private String message;
+    private String morse;
+
+}
